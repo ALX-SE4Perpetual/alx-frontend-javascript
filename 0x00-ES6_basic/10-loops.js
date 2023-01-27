dca91820e45b38ffe1b6dd1f6069f718393371dd
Perpy-del <perpetualmeninwa@gmail.com>
2023-01-27 12:02:25 +0000

    ES6 For/of Loop

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const arrValue = [];
+  for (const idx of array) {
+    arrValue.push(`${appendString}${idx}`);
+  }
+  return arrValue;
+}
