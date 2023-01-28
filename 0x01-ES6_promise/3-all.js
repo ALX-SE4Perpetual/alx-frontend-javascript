0074881690bd50daad88279e0db8b0dff278e9bf
Perpy-del <perpetualmeninwa@gmail.com>
2023-01-28 16:25:39 +0000

    Promises all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const p1 = uploadPhoto();
+  const p2 = createUser();
+
+  return Promises.all([p1, p2])
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    })
+    .catch(() => { console.log('Signup system offline'); 
+  });
+}
