f0baeb9047b8aa59a2bbf5ecea01428ccfa441e4
Perpy-del <perpetualmeninwa@gmail.com>
2023-01-31 10:48:01 +0000

    toString

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,11 +1,11 @@
 export default class Airport {
   constructor(name, code) {
     if (typeof name !== 'string') {
-      throw TypeError('Name should be a string');
+      throw Error('Name must be a string');
     }
 
     if (typeof code !== 'string') {
-      throw TypeError('Code should be a string');
+      throw Error('Code must be a string');
     }
 
     this._name = name;
